6262c23e0a0ede61ee7c1e72d592b7cd27f7cb7f
Seraphin Berger <82591988+MCmoderSD@users.noreply.github.com>
2023-04-28 10:31:54 +0200

    AutoUpdate Update.sh
    Auto Update

--- Update.sh
+++ Update.sh
@@ -1,1 +1,11 @@
-sudo git pull https://github.com/Redflame125/Capybara.git
+#!/bin/bash
+
+if [ "$1" = "-auto" ]; then
+  while true; do
+    sudo git pull https://github.com/Redflame125/Capybara.git
+    sleep 30
+  done
+else
+  sudo git pull https://github.com/Redflame125/Capybara.git
+  sudo systemctl reload apache2
+fi
